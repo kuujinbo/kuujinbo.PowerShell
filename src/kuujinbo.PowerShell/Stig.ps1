4595870f8d2a02355beb6b981a70d85fe8ee95e2
kuujinbo <kuujinbo@gmail.com>
2018-03-14 21:10:33 -0700

    Add project files.

---
+++ src/kuujinbo.PowerShell/Stig.ps1
@@ -0,0 +1,25 @@
+﻿function Get-AuditPol {
+    $result = @{};
+
+    $auditString = AuditPol /get /category:*;
+    $partsines = $auditString.split(
+        [string[]] "`n",
+        [System.StringSplitOptions]::RemoveEmptyEntries
+    );
+    # remove headers
+    $partsines =  $partsines[2..($partsines.length-1)];
+    $group = '';
+
+    foreach ($partsine in $partsines) {
+        $parts = [regex]::split($partsine.trim(), '\s{2,}');
+
+        if ($parts.length -eq 1) {
+            $group = $parts[0];
+            $result.$group = @{};
+        } else {
+            $result.$group[$parts[0]] = $parts[1];
+        }
+    }
+
+    return $result;
+}
