3b4308063c99a0dc1ccf4d2eba6af46a57828ad4
kuujinbo <kuujinbo@gmail.com>
2018-03-15 14:49:15 -0700

    parse rule file

--- src/kuujinbo.PowerShell/Stig.ps1
+++ src/kuujinbo.PowerShell/Stig.ps1
@@ -1,4 +1,58 @@
-﻿function Get-AuditPol {
+﻿function Parse-CheckContent {
+    param(
+        [Parameter(Mandatory=$true)] [string]$text
+        , [Parameter(Mandatory=$true)] [string]$rule
+    )
+
+    $lines = $text.Split(
+        [string[]] "`n", 
+        [StringSplitOptions]::RemoveEmptyEntries
+    );
+    $wanted = "^(?:registry|value|type)[^:]*:(?'value'.*)";
+    
+    <#
+        STIG authors **RIDICULOUSLY** inconsistent, but should be in groups of 5:
+        Registry Hive, Registry Path, Value Name, [Value] Type, Value
+    #>
+    $result = @();
+    foreach ($line in $lines) {
+        if ($line -match $wanted) {
+            $result += $matches['value'].Trim();
+        }
+    }
+    return $result;
+}
+
+
+function Get-Rules {
+    param(
+        [Parameter(Mandatory=$true)] [string]$filePath
+    )
+
+    [xml]$xmlRules = Get-Content -Path $filePath -ErrorAction Stop;
+    $result = @{};
+    if($xmlRules) {
+        $CATs = @{
+            low = 'CAT I';
+            medium = 'CAT II';
+            high = 'CAT III';
+        };
+
+        foreach ($group in $xmlRules.Benchmark.Group) {
+            foreach ($rule in $group.Rule) {
+                $result[$group.id] = New-Object -TypeName PSObject -Property (@{
+                    title = $rule.title;
+                    severity = $CATs[$rule.severity];
+                    registry = Parse-CheckContent $rule.check.'check-content' $group.id;
+                });
+            }
+        }
+        return $result;
+    }
+}
+
+
+function Get-AuditPol {
     # suppress STDERR if run with insufficient privileges
     $audit = AuditPol /get /category:* 2>$null;
     if (!$audit) { return $null; }
