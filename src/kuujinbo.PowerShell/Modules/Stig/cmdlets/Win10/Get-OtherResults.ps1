f9be779db9d7517a580ea8330fdfafb4dfe0e431
kuujinbo <kuujinbo@gmail.com>
2018-04-09 16:28:00 -0700

    more rules; User Rights Assignment

--- src/kuujinbo.PowerShell/Modules/Stig/cmdlets/Win10/Get-OtherResults.ps1
+++ src/kuujinbo.PowerShell/Modules/Stig/cmdlets/Win10/Get-OtherResults.ps1
@@ -1,28 +1,166 @@
 function Get-OtherResults {
     $results = @{};
 
+    $winVersion = Get-Win10Version;
+    if ($winVersion -ne $null) { $winVersion = [int]$winVersion; } 
+    else { $winVersion = 0; } 
+
+    $installedPrograms = Get-ItemProperty hklm:\software\microsoft\windows\currentversion\uninstall\*;
+
+
     $results = Get-EqualOrNoName;
     $results += Get-RegistryResults (Get-RegistryRulesRange) -invoke;
     $results += Get-V-63319;
     $results += Get-V-63323;
-    $results += Get-V-77083;
+    $results += Get-V-77083-77085;
+    $results += V-63349 $winVersion;
+    $results += V-63519-63523-63527 $installedPrograms;
+    $results += Get-V-63337;
+    $results += Get-V-63353 $installedPrograms;
 
     return $results;
 }
 
-function Get-V-77083 {
+<#
+.SYNOPSIS
+   Verify local volumes must be formatted using NTFS 
+#>
+function Get-V-63353 {
+    $drives = Get-Volume | where { $_.DriveType -eq 'Fixed' -and $_.DriveLetter; };
+    #         ^^^^^^^^^^ **ONLY** Win10 and above
+    $pass = $true;
+    $fail = @();
+    foreach ($d in $drives) {
+        if ($d.FileSystem -ne 'NTFS') {
+            $fail += $d.DriveLetter;
+            $pass = $false;
+        }
+    }
+
+    if ($pass) {
+        return @{ 'V-63353' = @($CKL_STATUS_PASS, 'All drive(s) formatted using the NTFS file system.'); };
+
+    } else {
+        return @{ 'V-63353' = @($CKL_STATUS_OPEN, "Drive(s) not formatted using the NTFS file system: $($a -join ',')"); };
+    }
+}
+
+<#
+.SYNOPSIS
+   Verify anti-virus status. 
+.TODO
+    Verify valid text match for installed anti-visrus program. (McAfee VirusScan Enterprise)
+#>
+function Get-V-63351 {
+    param($installedPrograms);
+    
+    $hasNA = $installedPrograms | where {$_.displayname -match 'McAfee Agent'};
+    if ($hasNA -ne $null) {
+        return @{ 'V-63351' = @($CKL_STATUS_PASS, 'Anti-virus solution is installed on the system'); };
+
+    } else {
+        return @{ 'V-63351' = @($CKL_STATUS_OPEN, 'Anti-virus solution is not installed on the system'); };
+    }
+}
+
+<#
+.SYNOPSIS
+   Verify BitLocker is enabled.
+#>
+function Get-V-63337 {
+    $bl = Get-BitlockerVolume -MountPoint "C:";
+    #     ^^^^^^^^^^^^^^^^^^^  **ONLY** Win10 and above
+    if ($bl -ne $null -and $bl.ProtectionStatus -eq 'On') {
+        return @{'V-63337' = @($CKL_STATUS_PASS, "BitLocker is enabled."); }
+    } else {
+        return @{'V-63337' = @($CKL_STATUS_OPEN, "BitLocker is not enabled."); }
+    }
+}
+
+<#
+.SYNOPSIS
+   Get V-63519, V-63523, and V-63527 results. 
+.NOTES
+    If the system is configured to send audit records directly to an audit
+    server, this is NA. This must be documented with the ISSO.    
+#>
+function V-63519-63523-63527 {
+    param($installedPrograms);
+    
+    $hasNA = $installedPrograms | where {$_.displayname -eq 'UniversalForwarder'};
+    if ($hasNA -ne $null) {
+        $passDetails = 'System configured to send audit records directly to an audit server using UniversalForwarder.'; 
+        return @{
+            'V-63519' = @($CKL_STATUS_NA, $passDetails); 
+            'V-63523' = @($CKL_STATUS_NA, $passDetails); 
+            'V-63527' = @($CKL_STATUS_NA, $passDetails); 
+        };
+
+    } else {
+        $rules = @{
+            'V-63519' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application\', 'MaxSize', '-ge 32768');
+            'V-63523' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Security\', 'MaxSize', '-ge 196608');
+            'V-63527' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System\', 'MaxSize', '-ge 32768');
+        };
+        return Get-RegistryResults $rules -invoke;
+    }
+}
+
+<#
+.SYNOPSIS
+   Verify Win10 version is at a supported servicing level 
+#>
+function V-63349 {
+    param([int]$actual = 0);
+
+    if ($actual -ge 1511) {
+        return @{'V-63349' = @($CKL_STATUS_PASS, "[$actual] Is a supported servicing level."); }
+    }
+    else {
+        return @{'V-63349' = @($CKL_STATUS_OPEN, "[$actual] Is not a supported servicing level."); }
+    }
+}
+
+<#
+.SYNOPSIS
+   Get V-77083 and V-77085 results. 
+.NOTES
+    `Confirm-SecureBootUEFI` cmdlet covers V-77083 and V-77085:
+    
+    https://docs.microsoft.com/en-us/powershell/module/secureboot/confirm-securebootuefi?view=win10-ps
+    -- If the computer supports Secure Boot and Secure Boot is enabled, 
+       this cmdlet returns $True.
+
+    -- If the computer supports Secure Boot and Secure Boot is disabled, 
+       this cmdlet returns $False.
+
+    -- If the computer does not support Secure Boot or is a BIOS (non-UEFI) computer, 
+       this cmdlet displays the following:
+#>
+function Get-V-77083-77085 {
     try {
         if (Confirm-SecureBootUEFI -ErrorAction Stop) {
-            return @{'V-77083' = @($CKL_STATUS_PASS, "Configured to run in UEFI mode."); };
+            return @{
+                'V-77083' = @($CKL_STATUS_PASS, "Configured to run in UEFI mode."); 
+                'V-77085' = @($CKL_STATUS_PASS, "Secure Boot enabled."); 
+            };
         }
     } catch {
         # TODO: research why this is a WTF 
         # Write-Output "$h => $($_.Exception.Message)";
         # Write-Output "$($_.Exception.Message)";
     }
-    return @{'V-77083' = @($CKL_STATUS_OPEN, "Not in UEFI mode."); }
+    return @{
+        'V-77083' = @($CKL_STATUS_OPEN, "Not in UEFI mode."); 
+        'V-77085' = @($CKL_STATUS_OPEN, "Secure Boot is not enabled."); 
+    }
 }
 
+
+<#
+.SYNOPSIS
+   Verify TPM enabled and ready for use.
+#>
 function Get-V-63323 {
     # not sure if native powershell cmdlet Get-Tpm has a way to get versions...
     $namespace = 'root\cimv2\security\microsofttpm';
@@ -45,6 +183,10 @@ function Get-V-63323 {
     return @{'V-63323' = @($CKL_STATUS_OPEN, "TPM not enabled."); }
 }
 
+<#
+.SYNOPSIS
+   Verify Windows 10 Enterprise Edition 64-bit version.
+#>
 function Get-V-63319 {
     $os = Get-WmiObject Win32_OperatingSystem;
     $arch = $os.OSArchitecture;
@@ -117,16 +259,6 @@ function Get-RegistryRulesTEST {
         'V-74725' = @('HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb10\', 'Start', '4');
         # NA prior to v1709 
         'V-77025' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\App and Browser protection\', 'DisallowExploitProtectionOverride', '1');
-
-
-
-        # If the system is configured to send audit records directly to an audit server, this is NA. This must be documented with the ISSO.
-        'V-63519' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application\', 'MaxSize', '0x00008000 (32768) (or greater)');
-        'V-63523' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Security\', 'MaxSize', '0x00030000 (196608) (or greater)');
-        'V-63527' = @('HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System\', 'MaxSize', '0x00008000 (32768) (or greater)');
-
-
-
 
         'V-63661' = @('HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\', 'MaximumPasswordAge', '30 (or less, excluding 0)');
         'V-74413' = @('HKLM:\SOFTWARE\Policies\Microsoft\ Cryptography\Configuration\SSL\00010002\', 'EccCurves', 'NistP384 NistP256');
